e42d1aa1895c78c974659ea51f94d63487ea4aef
Dmitrii Valiaev <65446812+xzer122@users.noreply.github.com>
2024-09-29 13:12:37 +0700

    initial commit

---
+++ src/main/java/com/example/sqlite_web/SqliteWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlite_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqliteWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqliteWebApplication.class, args);
+	}
+
+}
